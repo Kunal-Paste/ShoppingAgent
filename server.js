782fe63c8bb11e871366cb7c30cec3dfd9e86eba
Kunal-Paste <pastekunal6@gmail.com>
2025-12-09 19:28:12 +0530

    update server log message for clarity

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const app = require("./src/app");
 const connectDB = require("./src/db/db");
 
 connectDB();
+// new updation
 
 app.listen(3000, ()=>{
-    console.log("server is running at port 3000");
+    console.log("server is running at port 3000, will remove this tommorrow");
 })
