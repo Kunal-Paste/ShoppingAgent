b9ddd4c397ccd828ad75ec50e97e5bba2932557e
Kunal-Paste <pastekunal6@gmail.com>
2025-12-19 17:14:11 +0530

    feat: add update and delete product functionality; implement get products by seller

--- product/src/controllers/product.controller.js
+++ product/src/controllers/product.controller.js
@@ -1,3 +1,4 @@
+const { default: mongoose } = require('mongoose');
 const productModel = require('../model/product.model');
 const {uploadImage} = require('../services/imagekit.service');
 
@@ -78,8 +79,115 @@ async function getProductsById(req,res){
     })
 }
 
+async function updateProducts(req,res){
+
+    const {id} = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(400).json({
+            message:'invalid product id'
+        })
+    }
+
+    const product = await productModel.findOne({
+        _id:id,
+    })
+
+    if(!product){
+        return res.status(404).json({
+            message:'product not found'
+        })
+    }
+
+    if(product.seller.toString() !== req.user.id){
+        return res.status(403).json({
+            message:'forbidden: you can only update your product'
+        })
+    }
+
+    const allowedUpdates = ['title','description','price'];
+
+    for(const key of Object.keys(req.body)){
+        if(allowedUpdates.includes(key)){
+
+            if(key==='price' && typeof req.body.price === 'object'){
+
+                if(req.body.price.amount !== undefined){
+                    product.price.amount = Number(req.body.price.amount)
+                }
+
+                if(req.body.price.currency !== undefined){
+                    product.price.currency = req.body.price.currency
+                }
+
+            } else {
+                product[key] = req.body[key];
+            }
+        }
+    }
+
+    await product.save();
+
+    return res.status(200).json({
+        message:'product updated successfully',
+        product
+    })
+
+}
+
+async function deleteProducts(req,res){
+
+    const {id} = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(400).json({
+            message:'invalid products id'
+        })
+    }
+
+    const product = await productModel.findOne({
+        _id:id,
+    })
+
+    if(!product){
+        return res.status(404).json({
+            message:'product not found'
+        })
+    }
+
+    if(product.seller.toString() !== req.user.id){
+        return res.status(403).json({
+            message:'forbidden: you can only delete your product'
+        })
+    }
+
+    await productModel.findOneAndUpdate({_id:id});
+    
+    return res.status(200).json({
+        message:'product deleted successfully'
+    })
+
+}
+
+async function getProductsBySeller(req,res){
+    
+    const seller = req.user
+
+    const {skip=0, limit=20} = req.query;
+
+    const products = await productModel.find({seller:seller.id}).skip(skip).limit(Math.min(limit, 20));
+
+    return res.status(200).json({
+        data:products
+    })
+
+}
+
 module.exports = {
     createProduct,
     getProducts,
-    getProductsById
+    getProductsById,
+    updateProducts,
+    deleteProducts,
+    getProductsBySeller
 }
--- product/src/routes/product.routes.js
+++ product/src/routes/product.routes.js
@@ -8,8 +8,11 @@ const upload = multer({storage:multer.memoryStorage()});
 const router = express.Router();
 
 router.post("/", middleware.authProduct(['admin','seller','user']), upload.array('images',5), validator.productValidator, controller.createProduct);
+router.get("/seller",middleware.authProduct(["seller"]),controller.getProductsBySeller);
 router.get("/", controller.getProducts);
+router.patch("/:id",middleware.authProduct(["seller"]), controller.updateProducts);
+router.delete("/:id",middleware.authProduct(["seller"]), controller.deleteProducts);
+
 router.get("/:id", controller.getProductsById);
-router.patch("/:id",middleware.authProduct(["seller"]))
 
 module.exports = router;
