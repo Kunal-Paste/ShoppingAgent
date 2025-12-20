2a545e3fb634a99cf557010627c700de39b1541e
Kunal-Paste <pastekunal6@gmail.com>
2025-12-20 12:05:27 +0530

    fix(db): ensure successful connection message is logged and add newline at end of file

--- cart/src/db/db.js
+++ cart/src/db/db.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose');
 async function connectDB() {
     try {
         await mongoose.connect(process.env.MONGODB_URL);
+        console.log('connected to db successfully');
     } catch (error) {
         console.log('failed to connect Db of cart',error)
     }
 }
+
+module.exports = connectDB;
