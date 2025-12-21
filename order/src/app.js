9887a00935afe2f281b6f9e2891be82db0a3dc73
Kunal-Paste <pastekunal6@gmail.com>
2025-12-21 12:59:31 +0530

    feat(order): implement order model, routes, and controller for order management

--- order/src/app.js
+++ order/src/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const cookieparser = require('cookie-parser')
+const cookieparser = require('cookie-parser');
+const orderRoute = require('./router/order.routes')
 
 const app = express();
 app.use(express.json());
 app.use(cookieparser());
+
+app.use('/api/order', orderRoute);
 
 module.exports = app;
---
+++ order/src/model/order.model.js
@@ -0,0 +1,72 @@
+const mongoose = require('mongoose');
+
+const addressSchema = new mongoose.Schema({
+      street:String,
+      city:String,
+      state:String,
+      zip:String,
+      country:String,
+      isDefault:{type:Boolean, default:false}
+});
+
+const orderSchema = new mongoose.Schema({
+      user:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:true
+      },
+
+      items:[
+        {
+            product:{
+                type:mongoose.Schema.Types.ObjectId,
+                required:true
+            },
+
+            quantity:{
+                type:Number,
+                default:1,
+                min:1
+            },
+
+            price:{
+                amount:{
+                    type:Number,
+                    required:true
+                },
+
+                currency:{
+                    type:String,
+                    required:true,
+                    enum:["USD","INR"]
+                }
+            }
+        }
+      ],
+
+      status:{
+        type:String,
+        enum:['DELIVERED','PENDING','CANCELLED','CONFIREMED','SHIPPED']
+      },
+
+      totalPrice:{
+        amount:{
+            type:Number,
+            required:true
+        },
+
+        currency:{
+            type:String,
+            required:true,
+            enum:['USD','INR']
+        }
+      },
+
+      shippingAddress:{
+        type:addressSchema,
+        required:true
+      }
+}, {timestamps:true});
+
+const orderModel = mongoose.model('order',orderSchema);
+
+module.exports = orderModel;
