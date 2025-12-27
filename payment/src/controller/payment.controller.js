6881df99131c397d9327199c3f079e55bcef2895
Kunal-Paste <pastekunal6@gmail.com>
2025-12-27 18:35:12 +0530

    created a api that verifies a payment and finally payment section is complete

--- payment/src/controller/payment.controller.js
+++ payment/src/controller/payment.controller.js
@@ -20,7 +20,24 @@ async function createPayment(req,res){
             }
         });
 
-        
+        const price = orderResponse.data.order.totalPrice;
+
+        const order = await razorpay.orders.create(price);
+
+        const payment = await paymentModel.create({
+            order:orderId,
+            razorpayOrderId:order.id,
+            user:req.user.id,
+            price:{
+                amount:order.amount,
+                currency:order.currency
+            }
+        });
+
+        return res.status(201).json({
+            message:'payment initiated',
+            payment
+        })
 
     } catch (err) {
         console.log('error while creating payment', err);
@@ -31,6 +48,53 @@ async function createPayment(req,res){
     }
 }
 
+async function verifyPayment(req,res){
+    const {razorpayOrderId, paymentId, signature} = req.body;
+    const secret = process.env.RAZORPAY_KEY_SECRET
+
+    try {
+        const { validatePaymentVerification } = require('razorpay/dist/utils/razorpay-utils');
+        
+        const isValid = validatePaymentVerification({
+            order_id: razorpayOrderId,
+            payment_id: paymentId
+        }, signature, secret);
+
+        if(!isValid){
+            return res.status(400).json({
+                message:'invalid signature'
+            })
+        }
+
+        const payment = await paymentModel.findOne({razorpayOrderId, status:'PENDING'});
+
+        if(!payment){
+            return res.status(404).json({
+                message:'payment not found'
+            })
+        }
+
+        payment.paymentId = paymentId;
+        payment.signature = signature;
+        payment.status = 'COMPLETED'
+
+        await payment.save();
+
+        res.status(200).json({
+            message:'payment verified successfully',
+            payment
+        })
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message:'internal server error in verifying payment',
+            error:err.message
+        })
+    }
+}
+
 module.exports = {
-    createPayment
+    createPayment,
+    verifyPayment
 }
--- payment/src/routes/payment.routes.js
+++ payment/src/routes/payment.routes.js
@@ -5,5 +5,6 @@ const controller = require('../controller/payment.controller')
 const router = express.Router();
 
 router.post('/create/:orderId', middleware.authPayment(['user','seller']), controller.createPayment);
+router.post('/verify', middleware.authPayment(['user','seller']), controller.verifyPayment);
 
 module.exports = router;
