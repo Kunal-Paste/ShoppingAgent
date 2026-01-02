ef3792937ff59fe35ddfa393e10991162bd6f710
Kunal-Paste <pastekunal6@gmail.com>
2026-01-02 15:56:43 +0530

    added functions for publishing info to queue and subscribe i.e consuming message from queue

--- notification/src/broker/broker.js
+++ notification/src/broker/broker.js
@@ -16,8 +16,43 @@ async function connect(){
 
 }
 
+async function publishToQueue(queueName,data={}){
+
+    if(!channel || !connection) await connect();
+
+    await channel.assertQueue(queueName, {
+        durable:true
+    });
+
+    channel.sendToQueue(queueName, Buffer.from(JSON.stringify(data)));
+    console.log('message send to queue',queueName,data);
+    
+}
+
+async function subscribeToQueue(queueName,callback) {
+  
+    if(!channel || !connection) await connect();
+
+    await channel.assertQueue(queueName,{
+        durable:true
+    });
+
+    channel.consume(queueName, async(msg)=>{
+   
+        if(msg !== null){
+            const data = JSON.parse(msg.connect.toString());
+            await callback(data);
+            channel.ack(msg);
+        }
+
+    })
+
+}
+
 module.exports = {
     channel,
     connection,
-    connect
+    connect,
+    publishToQueue,
+    subscribeToQueue
 }
